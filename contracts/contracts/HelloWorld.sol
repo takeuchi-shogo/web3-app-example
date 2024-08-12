9cc8a7887ac262c918a69b3d6bba1fd70c5b5ed0
ShogoTakeuchi <59919423+takeuchi-shogo@users.noreply.github.com>
2024-08-12 10:29:37 +0900

    first commit

---
+++ contracts/contracts/HelloWorld.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.24;
+
+contract HelloWorld {
+	function getMessage() external pure returns (string memory) {
+		return "Hello World";
+	}
+}
